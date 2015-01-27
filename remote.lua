27bca0d9d6ed46851c82b4e623640a4e760df935
theroppex <theroppex@gmail.com>
2015-01-27 16:35:18 +0100

    Updated Remote

--- remote.lua
+++ remote.lua
@@ -1,9 +1,38 @@
 local keyboard = libs.keyboard;
 local task = libs.win;
 
-actions.switch = function()
-	task.switchtowait("Popcorn Time.exe");
+actions.launch = function()
+	if OS_WINDOWS then
+		os.execute("start \"\" \"%localappdata%\\Popcorn Time\\Popcorn Time\" ");
+		settings.state = "on";
+		layout.btnStart.color = "#960000";
 	end
+end
+
+actions.switch = function()
+	if OS_WINDOWS then
+		task.switchtowait("Popcorn Time.exe");
+	end
+end
+
+actions.exit = function()
+	if OS_WINDOWS then
+		actions.switch();
+		keyboard.stroke("menu", "f4");
+		settings.state = "off";
+		layout.btnStart.color = "#009600";
+	end
+end
+
+actions.tabL = function ()
+	actions.switch();
+	keyboard.stroke("shift", "tab");
+end
+
+actions.tabR = function ()
+	actions.switch();
+	keyboard.press("tab");
+end
 
 actions.back = function ()
 	actions.switch();
@@ -45,8 +74,22 @@ actions.down = function ()
 	keyboard.press("down");
 end
 
-actions.start = function ()
+actions.season_up = function ()
 	actions.switch();
-	keyboard.press("backspace");
+	keyboard.stroke("control", "up");
+end
+
+actions.season_down = function ()
+	actions.switch();
+	keyboard.stroke("control", "down");
+end
+
+actions.start = function ()
+	local state = settings.state;
+	if state == "off" then
+		actions.launch();
+	else
+		actions.exit();
+	end
 end
 
